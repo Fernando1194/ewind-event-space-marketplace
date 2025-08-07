a838155afd44fd242588abfc0cafd6f496f8c500
Fernando1194 <fernando.vaf94@gmail.com>
2025-08-07 10:03:21 -0300

    Add .next/static/chunks/src_app_venue_[id]_page_tsx_6dbed990._.js 1754571799897

---
+++ .next/static/chunks/src_app_venue_[id]_page_tsx_6dbed990._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_f5b37eb1._.js",
+  "static/chunks/node_modules_68b11941._.js"
+],
+    source: "dynamic"
+});
