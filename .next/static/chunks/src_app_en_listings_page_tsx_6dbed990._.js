a7fbdc21affd604f46b81537e71c30c8c9bf5029
Fernando1194 <fernando.vaf94@gmail.com>
2025-08-07 10:03:11 -0300

    Add .next/static/chunks/src_app_en_listings_page_tsx_6dbed990._.js 1754571790272

---
+++ .next/static/chunks/src_app_en_listings_page_tsx_6dbed990._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_0fdfe6ae._.js",
+  "static/chunks/node_modules_e1e094eb._.js"
+],
+    source: "dynamic"
+});
